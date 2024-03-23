a536d0d7d2c48aa758be934844093e531f056b56
a_cpp_coder <docdaodoi1@gmail.com>
2024-03-23 23:14:45 +0700

    the true meaning of srand

---
+++ random.rb
@@ -0,0 +1,15 @@
+srand 5
+puts rand(100)
+puts rand(100)
+puts rand(100)
+puts rand(100)
+puts rand(100)
+puts rand(100)
+puts ''
+
+srand 5
+puts rand(100)
+puts rand(100)
+puts rand(100)
+puts rand(100)
+puts rand(100)
